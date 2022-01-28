6265961a33a75570ed0412dd1eda87ec3b3d045b
Justin4587 <Thurman@atlasok.com>
2022-01-28 14:44:26 -0600

    Does close count

--- scripts.js
+++ scripts.js
@@ -47,6 +47,35 @@ function tutorialsBuilder(item, i) {
 </div>`)
 }
 
+function latestBuilder(item, i) {
+  let activeStatus = "";
+  if (!i) {
+    activeStatus = "active";
+  };
+  $("#carousel_latest").append(`
+  <div class="card carousel-item ${activeStatus} border-0" style="height: 335px; width: 255px">
+  <div id="video-card-image" class="d-block align-self-center justify-content-center" style="background-image: url(${item.thumb_url}); background-size: cover; background-position: center; height: 154px; width: 255px;">
+    <img src="./images/play.png" class="mx-auto my-5 d-block card-image align-self-center" style="width: 64px; height: 64px;">
+  </div>
+  <div class="card-body" style="height: 181px">
+    <h5 class="card-title m-0 p-0">${item.title}</h5>
+    <p class="card-text text-secondary">${item['sub-title']}</p>
+    <div class="d-flex justify-content-start">
+      <img src="${item.author_pic_url}" class="rounded-circle" width="30px" height="30px">
+      <p class="text-purple">${item.author}</p>
+    </div>
+    <div class="d-flex justify-content-space-between">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_off.png" width="15px" height="15px">
+      <p class="text-purple">${item.duration}</p>
+    </div>
+  </div>
+</div>`)
+}
+
 async function quotesData () {
   $.ajax({
     url: "https://smileschool-api.hbtn.info/quotes",
@@ -78,6 +107,22 @@ async function tutorialsData () {
   });
 }
 
+async function latestData () {
+  $.ajax({
+    url: "https://smileschool-api.hbtn.info/latest-videos",
+    method: "GET",
+    contentType: "application/json",
+    success: function (result) {
+      $("#carousel_latest").empty();
+      hideSpinner();
+      result.forEach((item, i) => {
+        console.log(i);
+        latestBuilder(item, i);
+      });
+    }
+  });
+}
+
 // Function to hide the Spinner
 function hideSpinner() {
 	document.getElementById('spinner').style.display = 'none';
@@ -86,4 +131,5 @@ function hideSpinner() {
 window.onload = () => {
   quotesData();
   tutorialsData();
+  latestData();
 }
