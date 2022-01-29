2184a14318a39c7f5e7a918354826eeee0593544
Justin4587 <Thurman@atlasok.com>
2022-01-28 23:39:35 -0600

    I really wanted to at least get some video cards to load on courses but hopefully it will be worth a couple points

--- scripts.js
+++ scripts.js
@@ -76,6 +76,35 @@ function latestBuilder(item, i) {
 </div>`)
 }
 
+function coursesBuilder(courses, i) {
+  let activeStatus = "";
+  if (!i) {
+    activeStatus = "active";
+  };
+  $("#coursesResults").append(`
+  <div class="card carousel-item ${activeStatus} border-0" style="height: 335px; width: 255px">
+  <div id="video-card-image" class="d-block align-self-center justify-content-center" style="background-image: url(${courses.thumb_url}); background-size: cover; background-position: center; height: 154px; width: 255px;">
+    <img src="./images/play.png" class="mx-auto my-5 d-block card-image align-self-center" style="width: 64px; height: 64px;">
+  </div>
+  <div class="card-body" style="height: 181px">
+    <h5 class="card-title m-0 p-0">${courses.title}</h5>
+    <p class="card-text text-secondary">${courses['sub-title']}</p>
+    <div class="d-flex justify-content-start">
+      <img src="${courses.author_pic_url}" class="rounded-circle" width="30px" height="30px">
+      <p class="text-purple">${courses.author}</p>
+    </div>
+    <div class="d-flex justify-content-space-between">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_on.png" width="15px" height="15px">
+      <img src="./images/star_off.png" width="15px" height="15px">
+      <p class="text-purple">${courses.duration}</p>
+    </div>
+  </div>
+</div>`)
+}
+
 async function quotesData () {
   $.ajax({
     url: "https://smileschool-api.hbtn.info/quotes",
@@ -123,6 +152,24 @@ async function latestData () {
   });
 }
 
+async function coursesData () {
+  $.ajax({
+    url: "https://smileschool-api.hbtn.info/courses",
+    method: "GET",
+    contentType: "application/json",
+    success: function (result) {
+      $("#coursesResults").empty();
+      const {courses} = result;
+      hideSpinner();
+      courses.forEach((courses, i) => {
+        console.log(courses);
+        console.log(result);
+        coursesBuilder(courses, i);
+      });
+    }
+  });
+}
+
 // Function to hide the Spinner
 function hideSpinner() {
 	document.getElementById('spinner').style.display = 'none';
@@ -132,4 +179,5 @@ window.onload = () => {
   quotesData();
   tutorialsData();
   latestData();
+  coursesData();
 }
